07623714421dfd1bfc8a0561399514f36900ef3a
emilecaron <emile.caron@outlook.com>
2014-06-04 15:28:40 +0200

    now disables proxy on failure. More verbose.

--- autoProxy.sh
+++ autoProxy.sh
@@ -1,7 +1,12 @@
  #!/bin/bash
 
 main(){
+    
+    export http_proxy=""
+    export https_proxy=""
+
     result=`HEAD -d -t 1 http://www.google.fr:80`
+
     if [ ! "$result" = "200 OK" ];then
         http_proxy_value="http://proxyweb.utc.fr:3128"
         https_proxy_value="https://proxyweb.utc.fr:3128"
@@ -9,7 +14,21 @@ main(){
         export http_proxy=$http_proxy_value  
         export https_proxy=$https_proxy_value  
         
-        echo "Proxy was turned on"
+        echo "No connection, trying proxy..."
+
+        result=`HEAD -d -t 1 http://www.google.fr:80`
+        if [ ! "$result" = "200 OK" ];then
+            
+            export http_proxy=""
+            export https_proxy=""
+            
+            echo "failed"
+            return
+        else
+            echo "success"
+        fi
+    else
+        echo "Connection ok"
     fi
 }
 
